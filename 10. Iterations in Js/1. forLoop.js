f2a3b08df7653267598b37d195b777a181fd27b6
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-07-25 19:25:05 +0500

    Defined for loop example

--- 10. Iterations in Js/1. forLoop.js
+++ 10. Iterations in Js/1. forLoop.js
@@ -10,3 +10,8 @@ for (initialization; condition; update) {
 for (let index = 1; index <= 10; index++) {
   console.log(index);
 }
+/*In the first iteration, the index is initialized, then the condition is checked, 
+if the condition check satisfies, code gets executed and the value of index is updated, 
+then again the condition is checked according to updated index and if it's true, the code executes and 
+this keeps on repeating until the condition check becomes false(condition is met).
+ */

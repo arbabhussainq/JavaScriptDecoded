9bdb57709f36313cb27b7453d5c28c2b0777bdbb
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-07-25 19:25:41 +0500

    Added loop nesting

--- 10. Iterations in Js/1. forLoop.js
+++ 10. Iterations in Js/1. forLoop.js
@@ -10,8 +10,17 @@ for (initialization; condition; update) {
 for (let index = 1; index <= 10; index++) {
   console.log(index);
 }
+
 /*In the first iteration, the index is initialized, then the condition is checked, 
 if the condition check satisfies, code gets executed and the value of index is updated, 
 then again the condition is checked according to updated index and if it's true, the code executes and 
 this keeps on repeating until the condition check becomes false(condition is met).
  */
+
+/*Nesting of Loops:
+Nesting of loops means placing one loop inside another loop. 
+The inner loop runs completely every time the outer loop runs once. 
+This is useful when working with multi-dimensional data, pattern printing, or performing repeated actions in layers.
+*/
+
+

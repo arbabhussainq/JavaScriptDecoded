b852f44feebf9b6571b6cd1ed119cdd6b7679320
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-07-25 19:23:50 +0500

    Intro to for loop

---
+++ 10. Iterations in Js/1. forLoop.js
@@ -0,0 +1,6 @@
+/* Iteration refers to the process of repeating a set of instructions, usually through loops until a certain condition is met. JavaScript supports several types of iteration mechanisms.
+SYNTAX:
+for (initialization; condition; update) {
+  // code
+}
+*/

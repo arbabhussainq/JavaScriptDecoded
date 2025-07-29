fb6b9dda5915a4d4d7263dc3233bd1630a093a44
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-07-29 19:34:15 +0500

    Update 2. whileLoop.js

--- 10. Iterations in Js/2. whileLoop.js
+++ 10. Iterations in Js/2. whileLoop.js
@@ -14,3 +14,11 @@ while (i <= 10) {
   console.log(`Value of i is ${i}`);
   i += 2;
 }
+
+// Iterating over an array using while loop:
+let myArray = ["Flash", "Batman", "SuperMan"];
+let index = 0;
+while (index < myArray.length) {
+  console.log(`Value at index ${index} is: ${myArray[index]}`);
+  index++;
+}

c18078a6bc35568adf3c5e9bc55421be7bd777cf
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-07-29 19:33:39 +0500

    Create 2. whileLoop.js

---
+++ 10. Iterations in Js/2. whileLoop.js
@@ -0,0 +1,11 @@
+/*
+While Loop:
+There are many ways of writing loops in programming, all having different syntax but the concept stays the same.
+Syntax:
+
+while (condition) {
+	// code to execute
+}
+
+Example:
+*/

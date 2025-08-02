94234955369329f9f7823eee812804e8ecb9fac0
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-08-02 20:23:41 +0500

    Update 3. doWhile.js

--- 10. Iterations in Js/3. doWhile.js
+++ 10. Iterations in Js/3. doWhile.js
@@ -3,3 +3,8 @@ Do...While Loop:
 A do...while loop is a variant of the while loop. The key difference is:
 It executes the loop body at least once, even if the condition is false.
 */
+let score = 11;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);

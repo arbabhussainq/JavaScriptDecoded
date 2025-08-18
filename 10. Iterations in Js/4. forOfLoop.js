2e015ea937574be2f3ea286670d5535b9d0f829e
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-08-18 19:22:15 +0500

    Update 4. forOfLoop.js

--- 10. Iterations in Js/4. forOfLoop.js
+++ 10. Iterations in Js/4. forOfLoop.js
@@ -9,3 +9,9 @@ Arrat of Objects: [ { }, { }, { } ]
 Example:
 */
 const arr = [1, 2, 3, 4, 5];
+/*
+for (const iterator of object) {
+  iterator is the same as index which will iterate over the object, object basically means 
+  the thing which you want to iterate over, It can be an object, String, Array etc.
+}
+*/

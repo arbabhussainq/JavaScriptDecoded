ba4311308fa014e89a299ec2a91352958236ec5d
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-08-18 19:20:21 +0500

    Create 4. forOfLoop.js

---
+++ 10. Iterations in Js/4. forOfLoop.js
@@ -0,0 +1,10 @@
+/*
+FOR OF LOOP:
+The `for...of` loop is a modern and clean way to ** iterate over iterable objects ** in JavaScript, such as:
+Arrays 
+Strings  
+Sets  
+Maps etc...
+Arrat of Objects: [ { }, { }, { } ]
+Example:
+*/

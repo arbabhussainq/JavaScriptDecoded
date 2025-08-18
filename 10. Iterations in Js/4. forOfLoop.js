7af456fcb7c361ccb0ab4b5573f204372d2103aa
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-08-18 19:22:47 +0500

    Update 4. forOfLoop.js

--- 10. Iterations in Js/4. forOfLoop.js
+++ 10. Iterations in Js/4. forOfLoop.js
@@ -18,3 +18,7 @@ for (const iterator of object) {
 for (const num of arr) {
   console.log(num);
 }
+const greetings = "Hello World";
+for (const greet of greetings) {
+  console.log(`Each char is ${greet}`);
+}

0cc5d486d72d1d02da7676b9944de414455fca5b
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-09-03 20:59:10 +0500

    Update 6. forEachLoop.js

--- 10. Iterations in Js/6. forEachLoop.js
+++ 10. Iterations in Js/6. forEachLoop.js
@@ -3,3 +3,4 @@ The forEach loop in JavaScript is a method used to iterate over arrays. It's cle
 readable, and a bit more elegant than traditional for loops, 
 especially when you just want to perform an operation on each element.
 */
+const coding = ["JavaScript", "Java", "Ruby", "Cpp", "Python"];

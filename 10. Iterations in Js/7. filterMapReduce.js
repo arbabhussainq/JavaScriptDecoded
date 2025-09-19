ac0e90ce0f5d8cc06b420336c0f4f8751e2eff9e
Arbab Hussain Qureshi <125045872+arbabhussainq@users.noreply.github.com>
2025-09-19 09:47:37 +0500

    Update 7. filterMapReduce.js

--- 10. Iterations in Js/7. filterMapReduce.js
+++ 10. Iterations in Js/7. filterMapReduce.js
@@ -1,3 +1,5 @@
 /* Filter in Js:
 Filter also takes a callback function and actually returns a value.It filters out the values according to a given condition.
 */
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
